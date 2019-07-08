7f52ca914254ead6bcd93d358481fe3fba577db9
pamenta <andi.pamenta@gmail.com>
2019-07-08 09:57:19 +0700

    Edit Suggestion when welcome

--- functions/index.js
+++ functions/index.js
@@ -138,7 +138,7 @@ const Contexts = {
 //                           "Do’a-do’a",
 //                           "Ayat-Ayat Tematik",
 //                           "Informasi Produk"
-                    );
+//                    );
 
 const BOOK_NAME = new Array("Surah Alfatihah",
                             "Surah Yasin",
