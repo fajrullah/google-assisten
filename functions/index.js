3100447593f27728e022f8a11a57588e5d25a5a6
pamenta <andi.pamenta@gmail.com>
2019-07-09 14:20:19 +0700

    it should works now

--- functions/index.js
+++ functions/index.js
@@ -17,35 +17,6 @@ const {
     Table
 } = require('actions-on-google');
 
-const smart_hafiz = 'smart hafiz';
-const hafiz_doll = 'hafiz doll';
-
-
-// Constants for selected item responses
-const PRODUCTS_RESPONSES = {
-    [smart_hafiz]: 'Anda memilih Smart Hafiz',
-    [hafiz_doll]: 'Anda memilih Hafiz Doll',
-};
-
-// Constants for list and carousel selection
-const SELECTION_KEY_GOOGLE_ASSISTANT = 'googleAssistant';
-const SELECTION_KEY_GOOGLE_PAY = 'googlePay';
-const SELECTION_KEY_GOOGLE_PIXEL = 'googlePixel';
-const SELECTION_KEY_GOOGLE_HOME = 'googleHome';
-
-// Constants for selected item responses
-const SELECTED_ITEM_RESPONSES = {
-  [SELECTION_KEY_GOOGLE_ASSISTANT]: 'Smart Hafiz',
-  [SELECTION_KEY_GOOGLE_PAY]: 'You selected Google Pay!',
-  [SELECTION_KEY_GOOGLE_PIXEL]: 'You selected Google Pixel!',
-  [SELECTION_KEY_GOOGLE_HOME]: 'You selected Google Home!',
-};
-
-
-const IMG_URL_AOG = 'https://storage.googleapis.com/actionsresources/logo_assistant_2x_64dp.png';
-const IMG_URL_GOOGLE_PAY = 'https://storage.googleapis.com/actionsresources/logo_pay_64dp.png';
-const IMG_URL_GOOGLE_PIXEL = "https://storage.googleapis.com/madebygoog/v1/Pixel/Pixel_ColorPicker/Pixel_Device_Angled_Black-720w.png";
-const IMG_URL_GOOGLE_HOME = "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw";
 
 const callFunction = require('./component/getEndingMessage');
 const {
@@ -120,81 +91,6 @@ app.intent('media status', (conv) => {
 });
 
 
-// Carousel
-app.intent('carousel', (conv) => {
-  conv.ask('This is an example of a carousel.');
-  conv.ask(new Suggestions(intentSuggestions));
-  conv.ask(new Carousel({
-    items: {
-      // Add the first item to the carousel
-      [SELECTION_KEY_GOOGLE_ASSISTANT]: {
-        synonyms: [
-          'Assistant',
-          'Google Assistant',
-        ],
-        title: 'Item #1',
-        description: 'Description of Item #1',
-        image: new Image({
-          url: IMG_URL_AOG,
-          alt: 'Google Assistant logo',
-        }),
-      },
-      // Add the second item to the carousel
-      [SELECTION_KEY_GOOGLE_PAY]: {
-        synonyms: [
-          'Transactions',
-          'Google Payments',
-      ],
-        title: 'Item #2',
-        description: 'Description of Item #2',
-        image: new Image({
-          url: IMG_URL_GOOGLE_PAY,
-          alt: 'Google Pay logo',
-        }),
-      },
-      // Add third item to the carousel
-      [SELECTION_KEY_GOOGLE_PIXEL]: {
-        synonyms: [
-          'Pixel',
-          'Google Pixel phone',
-        ],
-        title: 'Item #3',
-        description: 'Description of Item #3',
-        image: new Image({
-          url: IMG_URL_GOOGLE_PIXEL,
-          alt: 'Google Pixel phone',
-        }),
-      },
-      // Add last item of the carousel
-      [SELECTION_KEY_GOOGLE_HOME]: {
-        title: 'Item #4',
-        synonyms: [
-          'Google Home',
-        ],
-        description: 'Description of Item #4',
-        image: new Image({
-          url: IMG_URL_GOOGLE_HOME,
-          alt: 'Google Home',
-        }),
-      },
-    },
-  }));
-});
-
-
-// Handle list or carousel only
-app.intent('item selected', (conv, params, option) => {
-  let response = 'You did not select any item from the list or carousel';
-  if (option && SELECTED_ITEM_RESPONSES.hasOwnProperty(option)) {
-    response = SELECTED_ITEM_RESPONSES[option];
-  } else {
-    response = 'You selected an unknown item from the list or carousel';
-  }
-  conv.ask(response);
-  conv.ask(new Suggestions(intentSuggestions));
-});
-
-
 // Start List Product
 app.intent('quit_app', (conv) => {
     conv.ask('Kami menyediakan produk edukasi untuk anak yang dapat dilihat dibawah ini');
@@ -203,36 +99,55 @@ app.intent('quit_app', (conv) => {
         title: 'Produk Edukasi Anak Dari Alqolam',
         items: {
                 // First Item
-                { title: 'Smart Hafiz',
+                [SMART_HAFIZ] : synonyms: [
+                  'Smart',
+                  'smarthafiz',
+                ],
+                title: 'Smart Hafiz',
                 description: 'Smart Hafiz Deskripsi',
                 image: new Image({
                     url: 'https://www.gstatic.com/images/branding/product/2x/assistant_48dp.png',
                     alt: 'Google Assistant logo',
-                }), }
+                }),
             // Add the second item to the list
-                { title: 'hafiz doll',
+                [HAFIZ_DOLL]: 
+                [SMART_HAFIZ] : synonyms: [
+                  'Smart',
+                  'smarthafiz',
+                ],
+                title: 'hafiz doll',
                 description: 'hafiz doll bilingual description',
                 image: new Image({
                     url: 'https://www.gstatic.com/images/branding/product/2x/pay_48dp.png',
                     alt: 'Google Pay logo',
-                }), }
+                }),
             },
     }));
   
 });
 // End List Product
 
-// Handle list or carousel selection
-app.intent('pilih_produk', (conv, params, option) => {
-    let response = 'Silahkan Pilih Produk';
-    if (option && PRODUCTS_RESPONSES.hasOwnProperty(option)) {
-        response = PRODUCTS_RESPONSES[option];
-    } else {
-        response = 'Terimakasih, silahkan kembali lagi apabila Anda tertarik dengan produk kami';
-    }
-    conv.ask(response);
-    conv.ask(new Suggestions(intentSuggestions));
-});
+// Handle list
+app.intent('pilih_produk', (conv, input, option) => {
+  if (option === SMART_HAFIZ) {
+    conv.ask("ini produk smart hafiz"); // this Simple Response is necessary
+        conv.ask(new BasicCard({
+            image: new Image({
+                url: 'https://assets.alqolam.com/images/2019/04/08/5.jpg', //url of your image.
+                alt: 'Smart Hafiz',
+            }),
+            title: 'Smart Hafiz',
+            subtitle: 'Bermain & belajar bersama Smart Hafiz',
+            text: 'Produk Edukasi Visual untuk anak meningkatkan kecerdasan motorik dan dapat belajar bersama smart hafiz',
+            buttons: new Button({
+                title: 'Selengkapnya',
+                url: 'https://alqolam.com',
+            }),
+        }));
+  } else {
+    conv.ask('Number two is also a great choice!')
+  }
+})
 
 // Start Detail Product
 app.intent('detail_product', (conv) => {
