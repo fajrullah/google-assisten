35c22474a5c07fa902e3f0e4458c1d9803a48ca2
pamenta <andi.pamenta@gmail.com>
2019-07-15 10:46:38 +0700

    Add more surah

---
+++ functions/component/surahdata.js
@@ -0,0 +1,35 @@
+module.exports = {
+SURAH: {
+    alfatihah: {
+        surat: 'alfatihah',
+        name: 'Surah Al-Fatihah',
+        url: 'https://alqolam.sgp1.digitaloceanspaces.com/Syikh%20Misyari%20Rasyid/001%20Al%20Faatihah.mp3',
+        desc: 'Surah Al-Fatihah Ayat 1 - 7'
+    },
+    albaqarah: {
+        surat: 'albaqarah',
+        name: 'Surah Al Baqoroh',
+        url: 'https://alqolam.sgp1.digitaloceanspaces.com/Syikh%20Misyari%20Rasyid/002%20Al%20Baqoroh.mp3',
+        desc: 'Surah Al-Fatihah Ayat 1 - 286'
+    },
+    aliimran: {
+        surat: 'aliimran',
+        name: 'Surah Ali \'Imraan',
+        url: 'https://alqolam.sgp1.digitaloceanspaces.com/Syikh%20Misyari%20Rasyid/003%20AliImraan.mp3',
+        desc: 'Surah An-Nas Ayat 1 - 200'
+    },
+    annisaa: {
+        surat: 'annisaa',
+        name: 'Surah An Nisaa',
+        url: 'https://alqolam.sgp1.digitaloceanspaces.com/Syikh%20Misyari%20Rasyid/004%20An%20Nisaa.mp3',
+        desc: 'Surah An-Nisaa Ayat 1 - 176'
+    },
+    almaidah: {
+        surat: 'annisaa',
+        name: 'Surah An Maaidah',
+        url: 'https://alqolam.sgp1.digitaloceanspaces.com/Syikh%20Misyari%20Rasyid/004%20An%20Nisaa.mp3',
+        desc: 'Surah An-Nisaa Ayat 1 - 176'
+    },
+    
+},
+}
